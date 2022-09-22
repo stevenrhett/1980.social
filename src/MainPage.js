684e870d41f5ffe137a676fbea3a600fb2be558f
stevenrhett <johnsonsteven18@gmail.com>
2022-09-22 14:28:52 -0400

    test ssss

---
+++ src/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const MainPage = () => {
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Hello there</h1>
+                        <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+                            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        <button className="btn btn-outline btn-warning">click me!</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MainPage;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import NavBar from "./NavBar";
+import MainPage from "./MainPage";
+import Footer from "./Footer";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+      <NavBar/>
+      <MainPage/>
+      <Footer/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
